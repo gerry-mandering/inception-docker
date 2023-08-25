e659032da9d3982e77efc6dd9ab464a00da5db88
gerry-mandering <kimmen1119@gmail.com>
2023-08-25 20:50:53 +0900

    Feat: add symbolic linking to init_nginx.sh

--- srcs/requirements/nginx/tools/init_nginx.sh
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-mkdir -p $CERTS_
+# symbolic linking logs
+ln -sf /dev/stdout /var/log/nginx/access.log && \
+ln -sf /dev/stderr /var/log/nginx/error.log
 
+# generate openssl certificate
+mkdir -p $CERTS_
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout $CERTS_/$DOMAIN_NAME.key \
         -out $CERTS_/$DOMAIN_NAME.crt \
         -subj "/C=KR/L=Seoul/O=42Seoul/CN=$DOMAIN_NAME" \
         > /dev/null 2>&1
 
+# execute nginx
 nginx -g "daemon off;"
