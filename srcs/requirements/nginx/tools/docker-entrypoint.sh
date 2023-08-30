513edda622a00f658f421dd887cbc42b97fcdb06
gerry-mandering <kimmen1119@gmail.com>
2023-08-30 12:20:01 +0900

    Feat: add comment, remove http2 config in nginx.conf

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# symbolic linking logs
+# Symbolic linking logs
 ln -sf /dev/stdout /var/log/nginx/access.log && \
 ln -sf /dev/stderr /var/log/nginx/error.log
 
-# generate openssl certificate
+# Generate openssl certificate
 mkdir -p $CERTS_
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout $CERTS_/$DOMAIN_NAME.key \
@@ -13,5 +13,5 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -subj "/C=KR/L=Seoul/O=42Seoul/CN=$DOMAIN_NAME" \
         > /dev/null 2>&1
 
-# execute nginx
+# Execute nginx
 exec nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
-# set privileges
+# Set privileges
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
 
-# make directory for php7.4-fpm.pid
+# Make directory for php7.4-fpm.pid
 mkdir -p /run/php
 
-# install wp-cli
+# Install wp-cli
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null 2>&1
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+# Wordpress Installation
 if [ ! -f /var/www/html/wp-config.php ]; then
     wp core download --allow-root
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOSTNAME --allow-root
@@ -20,6 +21,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
-# execute php-fpm
+# Execute php-fpm
 exec php-fpm7.4 -F
 
